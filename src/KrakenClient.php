2b3566f5671a8a321af6615d485d25f5c71a13dc
dve <david.vanderelst@anyti.me>
2017-08-12 12:49:53 +0200

    Prepare kraken api methods in the client facade with a "not yet implemented" exception until they are  implemented

--- src/KrakenClient.php
+++ src/KrakenClient.php
@@ -54,6 +54,26 @@ class KrakenClient
         return new PublicTimeRequestModel($this->krakenAPI, $this->logger);
     }
 
+    public function publicAssets()
+    {
+        throw new \BadMethodCallException(__METHOD__ . ' not yet implemented');
+    }
+
+    public function publicAssetPairs()
+    {
+        throw new \BadMethodCallException(__METHOD__ . ' not yet implemented');
+    }
+
+    public function publicTicker()
+    {
+        throw new \BadMethodCallException(__METHOD__ . ' not yet implemented');
+    }
+
+    public function publicOHLC()
+    {
+        throw new \BadMethodCallException(__METHOD__ . ' not yet implemented');
+    }
+
     /**
      * @return PublicDepthRequestModel
      */
@@ -62,4 +82,98 @@ class KrakenClient
         return new PublicDepthRequestModel($this->krakenAPI, $this->logger);
     }
 
+    public function publicTrades()
+    {
+        throw new \BadMethodCallException(__METHOD__ . ' not yet implemented');
+    }
+
+    public function publicSpread()
+    {
+        throw new \BadMethodCallException(__METHOD__ . ' not yet implemented');
+    }
+
+    public function privateBalance()
+    {
+        throw new \BadMethodCallException(__METHOD__ . ' not yet implemented');
+    }
+
+    public function privateTradeBalance()
+    {
+        throw new \BadMethodCallException(__METHOD__ . ' not yet implemented');
+    }
+
+    public function privateOpenOrders()
+    {
+        throw new \BadMethodCallException(__METHOD__ . ' not yet implemented');
+    }
+
+    public function privateClosedOrders()
+    {
+        throw new \BadMethodCallException(__METHOD__ . ' not yet implemented');
+    }
+
+    public function privateQueryOrders()
+    {
+        throw new \BadMethodCallException(__METHOD__ . ' not yet implemented');
+    }
+
+    public function privateTradesHistory()
+    {
+        throw new \BadMethodCallException(__METHOD__ . ' not yet implemented');
+    }
+
+    public function privateQueryTrades()
+    {
+        throw new \BadMethodCallException(__METHOD__ . ' not yet implemented');
+    }
+
+    public function privateOpenPositions()
+    {
+        throw new \BadMethodCallException(__METHOD__ . ' not yet implemented');
+    }
+
+    public function privateLedgers()
+    {
+        throw new \BadMethodCallException(__METHOD__ . ' not yet implemented');
+    }
+
+    public function privateTradeVolume()
+    {
+        throw new \BadMethodCallException(__METHOD__ . ' not yet implemented');
+    }
+
+    public function privateAddOrder()
+    {
+        throw new \BadMethodCallException(__METHOD__ . ' not yet implemented');
+    }
+
+    public function privateCancelOrder()
+    {
+        throw new \BadMethodCallException(__METHOD__ . ' not yet implemented');
+    }
+
+    public function privateDepositMethods()
+    {
+        throw new \BadMethodCallException(__METHOD__ . ' not yet implemented');
+    }
+
+    public function privateDepositAddresses()
+    {
+        throw new \BadMethodCallException(__METHOD__ . ' not yet implemented');
+    }
+
+    public function privateWithdrawInfo()
+    {
+        throw new \BadMethodCallException(__METHOD__ . ' not yet implemented');
+    }
+
+    public function privateWithdraw()
+    {
+        throw new \BadMethodCallException(__METHOD__ . ' not yet implemented');
+    }
+
+    public function privateWithdrawStatus()
+    {
+        throw new \BadMethodCallException(__METHOD__ . ' not yet implemented');
+    }
 }
