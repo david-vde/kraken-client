1d821ba5d183b382bd9feef3325603f530cb9cbc
dve <david.vanderelst@anyti.me>
2017-08-16 22:17:56 +0200

    Add static functions helper to CurrencyPair class

--- src/Constant/CurrencyPair.php
+++ src/Constant/CurrencyPair.php
@@ -4,63 +4,80 @@ namespace DVE\KrakenClient\Constant;
 
 class CurrencyPair
 {
-    const BCHEUR   = 'BCHEUR';
-    const DASHEUR  = 'DASHEUR';
-    const DASHUSD  = 'DASHUSD';
-    const DASHXBT  = 'DASHXBT';
-    const EOSETH   = 'EOSETH';
-    const EOSEUR   = 'EOSEUR';
-    const EOSUSD   = 'EOSUSD';
-    const EOSXBT   = 'EOSXBT';
-    const GNOETH   = 'GNOETH';
-    const GNOEUR   = 'GNOEUR';
-    const GNOUSD   = 'GNOUSD';
-    const GNOXBT   = 'GNOXBT';
-    const USDTZUSD = 'USDTZUSD';
-    const XETCXETH = 'XETCXETH';
-    const XETCXXBT = 'XETCXXBT';
-    const XETCZEUR = 'XETCZEUR';
-    const XETCXUSD = 'XETCXUSD';
-    const XETHXXBT = 'XETHXXBT';
-    const XETHZCAD = 'XETHZCAD';
-    const XETHZEUR = 'XETHZEUR';
-    const XETHZGBP = 'XETHZGBP';
-    const XETHZJPY = 'XETHZJPY';
-    const XETHZUSD = 'XETHZUSD';
-    const XICNXETH = 'XICNXETH';
-    const XICNXXBT = 'XICNXXBT';
-    const XLTCXXBT = 'XLTCXXBT';
-    const XLTCZEUR = 'XLTCZEUR';
-    const XLTCZUSD = 'XLTCZUSD';
-    const XMLNXETH = 'XMLNXETH';
-    const XMLNXXBT = 'XMLNXXBT';
-    const XREPXETH = 'XREPXETH';
-    const XREPXXBT = 'XREPXXBT';
-    const XREPZEUR = 'XREPZEUR';
-    const XREPZUSD = 'XREPZUSD';
-    const XXBTZCAD = 'XXBTZCAD';
-    const XXBTZEUR = 'XXBTZEUR';
-    const XXBTZGBP = 'XXBTZGBP';
-    const XXBTZJPY = 'XXBTZJPY';
-    const XXBTZUSD = 'XXBTZUSD';
-    const XXDGXXBT = 'XXDGXXBT';
-    const XXLMXXBT = 'XXLMXXBT';
-    const XXLMZEUR = 'XXLMZEUR';
-    const XXLMZUSD = 'XXLMZUSD';
-    const XXMRXXBT = 'XXMRXXBT';
-    const XXMRZEUR = 'XXMRZEUR';
-    const XXMRZUSD = 'XXMRZUSD';
-    const XXRPXXBT = 'XXRPXXBT';
-    const XXRPZCAD = 'XXRPZCAD';
-    const XXRPZEUR = 'XXRPZEUR';
-    const XXRPZJPY = 'XXRPZJPY';
-    const XXRPZUSD = 'XXRPZUSD';
-    const XZECXXBT = 'XZECXXBT';
-    const XZECZEUR = 'XZECZEUR';
-    const XZECZUSD = 'XZECZUSD';
+    const BCHEUR   = 'BCH/EUR';
+    const DASHEUR  = 'DASH/EUR';
+    const DASHUSD  = 'DASH/USD';
+    const DASHXBT  = 'DASH/XBT';
+    const EOSETH   = 'EOS/ETH';
+    const EOSEUR   = 'EOS/EUR';
+    const EOSUSD   = 'EOS/USD';
+    const EOSXBT   = 'EOS/XBT';
+    const GNOETH   = 'GNO/ETH';
+    const GNOEUR   = 'GNO/EUR';
+    const GNOUSD   = 'GNO/USD';
+    const GNOXBT   = 'GNO/XBT';
+    const USDTZUSD = 'USDT/ZUSD';
+    const XETCXETH = 'XETC/XETH';
+    const XETCXXBT = 'XETC/XXBT';
+    const XETCZEUR = 'XETC/ZEUR';
+    const XETCXUSD = 'XETC/XUSD';
+    const XETHXXBT = 'XETH/XXBT';
+    const XETHZCAD = 'XETH/ZCAD';
+    const XETHZEUR = 'XETH/ZEUR';
+    const XETHZGBP = 'XETH/ZGBP';
+    const XETHZJPY = 'XETH/ZJPY';
+    const XETHZUSD = 'XETH/ZUSD';
+    const XICNXETH = 'XICN/XETH';
+    const XICNXXBT = 'XICN/XXBT';
+    const XLTCXXBT = 'XLTC/XXBT';
+    const XLTCZEUR = 'XLTC/ZEUR';
+    const XLTCZUSD = 'XLTC/ZUSD';
+    const XMLNXETH = 'XMLN/XETH';
+    const XMLNXXBT = 'XMLN/XXBT';
+    const XREPXETH = 'XREP/XETH';
+    const XREPXXBT = 'XREP/XXBT';
+    const XREPZEUR = 'XREP/ZEUR';
+    const XREPZUSD = 'XREP/ZUSD';
+    const XXBTZCAD = 'XXBT/ZCAD';
+    const XXBTZEUR = 'XXBT/ZEUR';
+    const XXBTZGBP = 'XXBT/ZGBP';
+    const XXBTZJPY = 'XXBT/ZJPY';
+    const XXBTZUSD = 'XXBT/ZUSD';
+    const XXDGXXBT = 'XXDG/XXBT';
+    const XXLMXXBT = 'XXLM/XXBT';
+    const XXLMZEUR = 'XXLM/ZEUR';
+    const XXLMZUSD = 'XXLM/ZUSD';
+    const XXMRXXBT = 'XXMR/XXBT';
+    const XXMRZEUR = 'XXMR/ZEUR';
+    const XXMRZUSD = 'XXMR/ZUSD';
+    const XXRPXXBT = 'XXRP/XXBT';
+    const XXRPZCAD = 'XXRP/ZCAD';
+    const XXRPZEUR = 'XXRP/ZEUR';
+    const XXRPZJPY = 'XXRP/ZJPY';
+    const XXRPZUSD = 'XXRP/ZUSD';
+    const XZECXXBT = 'XZEC/XXBT';
+    const XZECZEUR = 'XZEC/ZEUR';
+    const XZECZUSD = 'XZEC/ZUSD';
+
+    public static function getCurrencyPair($currencyPair)
+    {
+        return str_replace('/', '', $currencyPair);
+    }
 
     public static function getPriceCurrency($currencyPair)
     {
         return substr($currencyPair, strlen($currencyPair)-4, 3);
+    }
+
+    public static function getCurrency1($currencyPair)
+    {
+        $array = explode('/', $currencyPair);
+        return count($array) > 0 ? $currencyPair[0] : '';
+    }
+
+    public static function getCurrency2($currencyPair)
+    {
+        $array = explode('/', $currencyPair);
+        return count($array) > 1 ? $currencyPair[1] : '';
     }
 }
