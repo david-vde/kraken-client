2672a356a103645ba91d8aab7bd798ee8c8179c9
dve <david.vanderelst@anyti.me>
2017-08-18 22:31:56 +0200

    Add Currency constants class

---
+++ src/Constant/Currency.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace DVE\KrakenClient\Constant;
+
+class Currency
+{
+    const BCH = 'BCH';
+    const DASH = 'DASH';
+    const EOS = 'EOS';
+    const GNO = 'GNO';
+    const USDT = 'USDT';
+    const XETC = 'XETC';
+    const XETH = 'XETH';
+    const XICN = 'XICN';
+    const XLTC = 'XLTC';
+    const XMLN = 'XMLN';
+    const XREP = 'XREP';
+    const XXBT = 'XXBT';
+    const XXDG = 'XXDG';
+    const XXLM = 'XXLM';
+    const XXMR = 'XXMR';
+    const XXRP = 'XXRP';
+    const XZEC = 'XZEC';
+    const ZUSD = 'ZUSD';
+    const ZEUR = 'ZEUR';
+    const ZGBP = 'ZGBP';
+    const ZJPY = 'ZJPY';
+}
